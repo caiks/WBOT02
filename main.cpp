6f32de8b18f201365fda6dcef4febb9ce8a04377
Cliff McCartney <caiks@greenlake.co.uk>
2022-04-22 14:34:39 +0100

    image and screen tests

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #include <QApplication>
 #include <QLabel>
+#include <QScreen>
 
 using namespace Alignment;
 using namespace WBOT02;
@@ -12,6 +13,7 @@ using namespace std;
 #define ECHO(x) cout << #x << endl; x
 #define ECHOT(x) cout << #x ; mark = Clock::now(); x ; cout << "\t" << ((Sec)(Clock::now() - mark)).count() << "s" << endl
 #define EVAL(x) cout << #x << ": " << (x) << endl
+#define EVALH(x) cout << #x << ": " << hex << (x) << dec << endl
 #define EVALL(x) cout << #x << ": " << endl << (x) << endl
 #define TRUTH(x) cout << #x << ": " << ((x) ? "true" : "false") << endl
 
@@ -24,9 +26,9 @@ int main(int argc, char *argv[])
 	
 	if (argc >= 3 && std::string(argv[1]) == "image001")
 	{
+		auto mark = Clock::now();
 		QApplication a(argc, argv);
 		QImage image;
-		auto mark = Clock::now();
 		ECHOT(image.load(argv[2]));
 		EVAL(image.format());
 		EVAL(image.depth());
@@ -35,18 +37,91 @@ int main(int argc, char *argv[])
 		EVAL(image.dotsPerMeterX());
 		EVAL(image.dotsPerMeterY());
 		ECHO(auto colour = image.pixel(QPoint(0,0)));
-		EVAL(colour);
-		EVAL(*(std::uint32_t*)image.constBits());
+        EVALH(colour);
+        EVALH(*(std::uint32_t*)image.constBits());
+        EVALH(*(QRgb*)image.constBits());
 		EVAL(qAlpha(colour));
 		EVAL(qRed(colour));
 		EVAL(qGreen(colour));
 		EVAL(qBlue(colour));
+        EVAL(qGray(colour));
+        EVAL((qRed(colour)+qGreen(colour)+qBlue(colour))/3);
 		// ECHO(image.fill(colour));
+		{
+			mark = Clock::now();
+			std::size_t total = 0;
+			std::size_t size = image.sizeInBytes()/4;
+			auto rgb = (QRgb*)image.constBits();
+			for (std::size_t x = 0; x < size; x++) 
+			{
+				total += qRed(*rgb) + qGreen(*rgb) + qBlue(*rgb);
+				rgb++;
+			}
+			EVAL(total);
+			total /= 3;
+			total /= size;
+			cout << "average per pixel:" << total << "\t" << ((Sec)(Clock::now() - mark)).count() << "s" << endl;
+		}
+		{
+			mark = Clock::now();
+			std::size_t total = 0;
+			for (int y = 0; y < image.height(); ++y) 
+			{
+				QRgb *line = reinterpret_cast<QRgb*>(image.scanLine(y));
+				for (int x = 0; x < image.width(); ++x) {
+					QRgb &rgb = line[x];
+					total += qRed(rgb) + qGreen(rgb) + qBlue(rgb);
+				}
+			}
+			EVAL(total);
+			total /= 3;
+			total /= image.height();
+			total /= image.height();
+			cout << "average per pixel:" << total << "\t" << ((Sec)(Clock::now() - mark)).count() << "s" << endl;
+		}
 		QLabel myLabel;
 		ECHOT(auto pixmap = QPixmap::fromImage(image));
 		ECHOT(myLabel.setPixmap(pixmap));
 		myLabel.show();
 		EVAL(a.exec());
+	}
+	
+	if (argc >= 2 && std::string(argv[1]) == "screen001")
+	{
+		auto mark = Clock::now();
+        QGuiApplication application(argc, argv);
+		QScreen *screen = QGuiApplication::primaryScreen();
+		ECHOT(auto pixmap = screen->grabWindow(0));
+		ECHOT(auto image = pixmap.toImage());
+		EVAL(image.format());
+		EVAL(image.depth());
+		EVAL(image.width());
+		EVAL(image.height());
+		EVAL(image.dotsPerMeterX());
+		EVAL(image.dotsPerMeterY());
+		ECHO(auto colour = image.pixel(QPoint(0,0)));
+        EVALH(colour);
+		EVAL(qAlpha(colour));
+		EVAL(qRed(colour));
+		EVAL(qGreen(colour));
+		EVAL(qBlue(colour));
+        EVAL(qGray(colour));
+        EVAL((qRed(colour)+qGreen(colour)+qBlue(colour))/3);
+		{
+			mark = Clock::now();
+			std::size_t total = 0;
+			std::size_t size = image.sizeInBytes()/4;
+			auto rgb = (QRgb*)image.constBits();
+			for (std::size_t x = 0; x < size; x++) 
+			{
+				total += qRed(*rgb) + qGreen(*rgb) + qBlue(*rgb);
+				rgb++;
+			}
+			EVAL(total);
+			total /= 3;
+			total /= size;
+			cout << "average per pixel:" << total << "\t" << ((Sec)(Clock::now() - mark)).count() << "s" << endl;
+		}
 	}
 	
 	if (argc >= 2 && std::string(argv[1]) == "win001")
